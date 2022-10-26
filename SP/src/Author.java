a34dbef2f1bfe8243380a961a822ea0dd7fb97be
Ionut Nahoi <80223790+ionutnahoi@users.noreply.github.com>
2022-10-26 08:13:47 +0300

    Add files via upload

---
+++ SP/src/Author.java
@@ -0,0 +1,12 @@
+public class Author {
+    private String name;
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public void print() {
+        System.out.println( "Author:" + name);
+    }
+
+}
